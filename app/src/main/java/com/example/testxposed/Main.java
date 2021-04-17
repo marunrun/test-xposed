8fbca9b72ab931e52e2d020026133dd3fabb763a
marun <marunrun@163.com>
2021-04-17 17:36:40 +0800

    add sekirco

--- app/src/main/java/com/example/testxposed/Main.java
+++ app/src/main/java/com/example/testxposed/Main.java
@@ -1,22 +1,26 @@
 package com.example.testxposed;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
+import android.app.AndroidAppHelper;
 import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 
 import com.sky.xposed.common.util.Alog;
 import com.sky.xposed.javax.MethodHook;
-import com.sky.xposed.javax.XposedPlus;
-import com.sky.xposed.javax.XposedUtil;
+import com.virjar.sekiro.api.SekiroClient;
+import com.virjar.sekiro.api.SekiroRequest;
+import com.virjar.sekiro.api.SekiroRequestHandler;
+import com.virjar.sekiro.api.SekiroResponse;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
 
 public class Main implements IXposedHookLoadPackage, MethodHook.ThrowableCallback {
 
@@ -33,6 +37,7 @@ public class Main implements IXposedHookLoadPackage, MethodHook.ThrowableCallbac
         if (!Constant.ShowStart.PACKAGE_NAME.equals(lpparam.packageName)) return;
         XposedHelpers.findAndHookMethod("com.stub.StubApp", lpparam.classLoader,
                 "ᵢˋ", Context.class, new XC_MethodHook() {
+                    @SuppressLint("PrivateApi")
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                         super.afterHookedMethod(param);
@@ -92,6 +97,108 @@ public class Main implements IXposedHookLoadPackage, MethodHook.ThrowableCallbac
                             }
                         });
 
+//antServer
+/*                        @RestController
+                        class MyController {
+
+                            @PostMapping("/sign")
+                            public String sign() {
+                                Class<?> clazzJDUtils = null;
+                                try {
+                                    clazzJDUtils = classLoader.loadClass("android.widget.Security");
+                                    XposedBridge.log("load class:" + clazzJDUtils);
+                                } catch (Exception e) {
+                                    XposedBridge.log("load class err:" + Log.getStackTraceString(e));
+                                }
+
+                                String rc = (String) XposedHelpers.callStaticMethod(clazzJDUtils, "restoreString", "123");
+                                XposedBridge.log("getSignFromJni = " + rc);
+                                return rc;
+                            }
+
+                        }
+
+                        Server server = AndServer.webServer(context)
+                                .port(8080)
+                                .timeout(10, TimeUnit.SECONDS)
+                                .build();
+
+// startup the server.
+                        server.startup();*/
+
+                        // NanoHttp server
+/*                        class myHttpServer extends NanoHTTPD {
+                            private static final String REQUEST_ROOT = "/";
+
+                            public myHttpServer() throws IOException {
+                                // 端口是8088，也就是说要通过http://127.0.0.1:8088来访当问
+                                super(1111);
+                                start(NanoHTTPD.SOCKET_READ_TIMEOUT, true);
+                                XposedBridge.log("---xposed Server---");
+                            }
+
+                            @Override
+                            public Response serve(IHTTPSession session) {
+                                // log("serve");
+                                //这个就是之前分析，重写父类的一个参数的方法，
+                                //这里边已经把所有的解析操作已经在这里执行了
+                                return super.serve(session);
+                            }
+
+                            @Override
+                            public Response serve(String uri, Method method, Map<String, String> headers, Map<String, String> parms, Map<String, String> files) {
+
+                                XposedBridge.log(uri);
+
+                                Class<?> clazzZy;
+                                try {
+                                    clazzZy = classLoader.loadClass("android.widget.Security");
+                                    XposedBridge.log("load class:" + clazzZy);
+                                } catch (Exception e) {
+                                    XposedBridge.log("load class err:" + Log.getStackTraceString(e));
+                                    return newFixedLengthResponse("load class is null");
+                                }
+                                if (uri.contains("sign")) {
+                                    XposedBridge.log(parms.get("data"));
+                                    return sign(clazzZy, parms.get("data"));
+                                }
+
+
+                                return super.serve(uri, method, headers, parms, files);
+                            }
+
+                            public Response sign(Class<?> clazzUse, String strData) {
+                                String rc = (String) XposedHelpers.callStaticMethod(clazzUse, "restoreString", strData);
+                                XposedBridge.log("sign = " + rc);
+                                return newFixedLengthResponse(rc);
+                            }
+                        }
+
+                        new myHttpServer();*/
+
+
+                        // Sekiro
+                        String strANDROID_ID =  "mi5";//Settings.System.getString(AndroidAppHelper.currentApplication().getApplicationContext().getContentResolver(), Settings.System.ANDROID_ID); // UUID.randomUUID().toString();  // Settings.System.getString(AndroidAppHelper.currentApplication().getApplicationContext().getContentResolver(), Settings.System.ANDROID_ID);
+
+                        String testHost = "marun.run";
+                        //接口组名称
+                        String groupName = "showstart";
+                        //暴露的接口名称
+                        String actionName = "getsign";
+
+                        SekiroClient.start(testHost, strANDROID_ID, groupName)
+                                .registerHandler(actionName, new SekiroRequestHandler() {
+                                    @Override
+                                    public void handleRequest(SekiroRequest sekiroRequest, SekiroResponse sekiroResponse) {
+                                        try {
+                                            Class<?> aClass = classLoader.loadClass("android.widget.Security");
+                                            String res = (String) XposedHelpers.callStaticMethod(aClass, "restoreString", sekiroRequest.getString("data"));
+                                            sekiroResponse.success(res);
+                                        } catch (ClassNotFoundException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                });
                     }
                 });
     }
--- app/src/main/java/com/example/testxposed/dialog/PluginDialog.java
+++ app/src/main/java/com/example/testxposed/dialog/PluginDialog.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.testxposed.ActivityPlugin;
@@ -17,6 +18,8 @@ import com.sky.xposed.common.ui.view.EditTextItemView;
 import com.sky.xposed.common.ui.view.SwitchItemView;
 import com.sky.xposed.common.ui.view.TitleView;
 import com.sky.xposed.common.util.DisplayUtil;
+
+import de.robv.android.xposed.XposedHelpers;
 
 public class PluginDialog extends BaseDialogFragment {
 
@@ -51,11 +54,6 @@ public class PluginDialog extends BaseDialogFragment {
         int left = DisplayUtil.dip2px(getContext(), 15);
         int top = DisplayUtil.dip2px(getContext(), 12);
 
-//        tvPrompt = new TextView(getContext());
-//        tvPrompt.setTextSize(14);
-//        tvPrompt.setBackgroundColor(Color.GRAY);
-//        tvPrompt.setTextColor(Color.WHITE);
-//        tvPrompt.setPadding(left, top, left, top);
         timerEnable = ViewUtil.newSwitchItemView(getContext(), "定时下单");
         timerEnable.setDesc("开启后，每次下单都会保存下来，定时再发送，为了抢票");
 
@@ -73,6 +71,8 @@ public class PluginDialog extends BaseDialogFragment {
         timerMinute.setUnit("分");
         timerMinute.setName("定时下单");
         timerMinute.setExtendHint("单位(分)");
+
+
 //        frameView.addContent(tvPrompt);
         frameView.addContent(timerEnable);
 //        frameView.addContent(timerHour);
@@ -82,8 +82,6 @@ public class PluginDialog extends BaseDialogFragment {
 
     @Override
     protected void initView(View view, Bundle args) {
-
-
         timerEnable.bind(getDefaultSharedPreferences(), Integer.toString(Constant.XFlag.ENABLE_TIMER), false, (view1, key, value) -> {
             plugin.setEnable(value);
             return true;
